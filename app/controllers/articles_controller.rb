5cfb6f1a3943f232a831a4a849791dd38a75a2b3
Nilamadhab Ghosh <nilamadhab.ghosh.2@gmail.com>
2019-10-29 02:45:56 -0500

    Edit and Index Added

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,14 @@
 class ArticlesController < ApplicationController
+    
+    def index
+      @articles = Article.all
+    end
     def new
         @article = Article.new
+    end
+
+    def edit
+      @article = Article.find(params[:id])
     end
 
     def create
@@ -14,6 +22,16 @@ class ArticlesController < ApplicationController
       end
       #render plain: params[:article].inspect
       #redirect_to article_path(@article)
+    end
+
+    def update
+      @article = Article.find(params[:id])
+      if @article.update(article_params)
+        flash[:notice] = "Article has been successfully updated"
+        redirect_to article_path(@article)
+      else
+        render 'new'
+      end 
     end
 
     def show
