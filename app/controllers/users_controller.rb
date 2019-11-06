3eef526b65b7f8108ea664c5bebbd386559ed5cf
Nilamadhab Ghosh <nilamadhab.ghosh.2@gmail.com>
2019-11-06 12:21:09 +0530

    updated

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,39 @@
+class UsersController < ApplicationController
+    def new
+        @user = User.new
+    end
+    def create
+      @user = User.new(user_params)
+      if @user.save
+        flash[:success] = "Welcome to Alpha Blog     #{@user.username}"
+        redirect_to articles_path
+      else
+        render 'new'
+      end
+    end
+    def edit
+        @user = User.find(params[:id])
+    end
+    def update
+        @user = User.find(user_params)
+        if @user.update
+            flash[:success] = "Your details has been successfully updated"
+            redirect_to article_path
+        else
+            render 'edit'
+        end
+    end
+
+    def show
+       @user = User.find(params[:id])
+    end
+
+    def index
+        @users = User.all 
+    end
+
+    private
+      def user_params
+        params.require(:user).permit(:username,:email,:password)
+      end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   root to:'pages#home'
   get 'about',to: 'pages#about'
   resources :articles
+
+  get 'signup',to: 'users#new'
+  resources :users,except:[:new]
 end
